ea2929cab809137c566278cb34cfdd1a67b1881c
unknown <galaxy.net0710@gmail.com>
2020-01-25 19:21:46 -0600

    Arreglando el fetch

---
+++ src/components/api.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Api(props) {
+
+  //   function all(data) {
+  //     data.forEach((element) => {
+
+  //       url = element.url;
+  //       fetch(url)
+  //         .then((response) => response.json())
+  //         .then((pokeData) => {
+  //           console.log(pokeData);
+  //         });
+
+  //     });
+  //     return element;
+  //   };
+  // eslint-disable-next-line react/destructuring-assignment
+  //all(props.datos);
+  // eslint-disable-next-line react/destructuring-assignment
+  console.log(props.datos);
+  return <h1>jajaja</h1>;
+
+}
+
+export default Api;
--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { useFetch } from './hooks';
+import Api from './api';
 
 function Pokemon() {
   const [data, loading] = useFetch(
     'https://pokeapi.co/api/v2/pokemon?limit=30',
-    // 'https://pokeapi.co/api/v2/pokemon/1',
+    //'https://pokeapi.co/api/v2/pokemon/1',
   );
-  console.log(data);
+  console.log(data.results);
 
   return (
     <>
-      <h1>Photos</h1>
-      {loading ? (
-        'Loading...'
-      ) : (
-        <ul>
-          <img src={data.sprites.front_default} alt='pokeimg' />
-          {/* {data.map((img) => (
-            <li>
-              <img alt='pomkemon' src={img.sprites.front_default} />
-            </li>
-          ))} */}
-        </ul>
-      )}
+      <div>
+        {loading ? (
+          'Loading...'
+        ) : (
+          <ul>
+            {/* <img src={data.sprites.front_default} alt='pokeimg' /> */}
+            {data.results.map((img) => (
+              <li>
+                <p>{img.url}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+        <Api datos={data.results} />
+      </div>
     </>
   );
 }
