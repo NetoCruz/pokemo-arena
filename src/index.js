ac22951b9402546117d7187134a261e858b7a0db
unknown <galaxy.net0710@gmail.com>
2020-02-01 14:14:34 -0600

    Mejoras al fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/Helloworld';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<HelloWorld />, document.getElementById('app'));
